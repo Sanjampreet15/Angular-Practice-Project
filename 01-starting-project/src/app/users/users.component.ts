25360f306de6e8bf61ef6720793a8b9267399f8b
Sanjam Kaur <sanjam.kaur@nagarro.com>
2024-09-04 23:19:32 +0530

    signal code example

--- 01-starting-project/src/app/users/users.component.ts
+++ 01-starting-project/src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS} from '../dummy.users';
 
 const random_Index = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,17 +11,19 @@ const random_Index = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-selectedUser = DUMMY_USERS[random_Index];
+selectedUser = signal(DUMMY_USERS[random_Index]);
+imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
 // getter property use to fetch the data
-get imagePath(){
-  return 'assets/users/' + this.selectedUser.avatar;
-}
+// get imagePath(){
+//   return 'assets/users/' + this.selectedUser.avatar;
+// }
 
 //Event listener
 onSelectUser() {
   console.log('Clicked!')
   const random_Index = Math.floor(Math.random() * DUMMY_USERS.length)
-  this.selectedUser = DUMMY_USERS[random_Index]
+  // this.selectedUser = DUMMY_USERS[random_Index]
+  this.selectedUser.set(DUMMY_USERS[random_Index])
 }
 }
